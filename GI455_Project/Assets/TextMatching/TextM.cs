f94b10b9ad12571ded9c4fac7284caa211478b60
unknown <sumet.sing@bumail.net>
2021-01-28 22:15:39 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMatching/TextM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextM : MonoBehaviour
+{
+    public InputField search;
+    public Text show;
+ 
+
+    public void Textsearch()
+    {
+        
+        if (search.text == "IU")
+        {
+            show.text = search.text +  "  is found.";
+        }
+        else if (search.text == "Sodiac")
+        {
+            show.text = search.text + "  is found.";
+        }
+        else if (search.text == "Hawk")
+        {
+            show.text = search.text + "  is found.";
+        }
+        else if (search.text == "Meliodas")
+        {
+            show.text = search.text + "  is found.";
+        }
+        else if (search.text == "Rimuru")
+        {
+            show.text = search.text + "  is found.";
+        }
+        else
+        {
+            show.text = search.text + "  not found.";
+        }
+
+    }
+
+}
